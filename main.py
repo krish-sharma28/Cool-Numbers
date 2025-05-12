7c31b05a4623f6c2d6511ff0749e4189c71cb0ca
jordyonetap <jroobennett@gmail.com>
2025-05-12 12:56:29 -0400

    Fixed edge cases, readability. Rules now in added txt file.

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ from creation_and_rounds_mode import Creation, card, play_game
 from leaderboard import show_leaderboard
 
 
-def main():
-    """ Switching name to intro, this is the start and setup of the code.
+def intro():
+    """ Logic for the introduction to the game. Allows user to pick between
+    leaderboard, rules, play, or exit.
 
+    
     
     """
     key = 0
-    welcome_string = """Welcome to 24!\n 
-The game of adding, subtracting, multiplying, and dividing\
-4 numbers to reach 24."""
+    welcome_string = """========================================================
+Welcome to 24!\n 
+The game of adding, subtracting, multiplying, 
+and dividing 4 numbers to reach 24.
+========================================================\n"""
 
     # handle logic for leaderboard, help, and play
     while key == 0:
@@ -27,79 +31,41 @@ The game of adding, subtracting, multiplying, and dividing\
     "- play\n"
     "- exit\n")
         if gather.lower() == 'leaderboard':
-            mode=input("Which mode leaderboard do you want to see?")
-            show_leaderboard(mode.lower())
-        elif gather.lower() == 'rules':
+            mode = input("Do you want to see Timed or Rounds leaderboard? ")
+            try:
+                show_leaderboard(mode.lower())
+            except:
+                print("Not a mode! Try Timed or Rounds")
+                intro()
+        elif gather.lower() == 'rules' or gather.lower() == 'help':
             help()
         elif gather.lower() == 'play':
-            mode=input("Choose the mode you want to play: Timed or Rounds ")
+            mode = input("Which to play: Timed or Rounds? ")
             if mode.lower()=="rounds":
                 players, rounds = card()
                 key = 1
                 play_game(players, rounds)
             elif mode.lower()=="timed":
                 timed_mode()
+            else:
+                print("Not a mode! Try Timed or Rounds")
             break
         elif gather.lower() == 'exit':
             return print("Exiting program.")
+            break
         else:
-            print("""Not a valid input! Choose between leaderboard, rules, 
-and play. Write 'exit' at any time to end program.""")
-    # while key == 1:
-    #     print("-----------------")
-    #     print(f"| {c_result[0]} | {c_result[1]} | {c_result[2]} | {c_result[3]} |")
-    #     print("-----------------")
-    #     gather = input("Enter your answer in reverse polish notation. ")
+            print(f"{gather} is not a recognized option!\n")
 
-    #     if score(gather):
-    #         print("Yippee!")
-    #         key == 0
-    #     elif gather.lower() == 'exit':
-    #         return print("Exiting program.")
-    #     else:
-    #         print("Incorrect! Try Again!")
-    #         pass
-
-def leaderboard():
-    pass
 
 def help():
-    """ Enters the help window, presenting the user with rules and a video.
-    
-    Side Effects:
-    if "more" is typed, player is brought to a video in their browser. 
-    """
     site = "https://vimeo.com/1013357391"
-    help_string = """
-    24 is played by taking 4 numbers and using basic arithmetic operations to
-    get to the number 24.\n
+    with open("helpstring.txt", "r") as file:
+        contents = file.read()
+        print(contents)
     
-    After typing 'play' into the prompt box, you will be asked 
-    for the mode you want to play:\n
-    Timed or Rounds\n
-    
-    After you choose the mode you want to play, you will be greeted by a hand.
-    ----------------- 
-    | 2 | 3 | 2 | 4 |  
-    -----------------  
-    To play you must type in your answer.
-    EXAMPLE: (2+2+4)*3
-    Still dont understand? To watch a video on the rules, type "more".
-    Otherwise, type 'return' """
-    print(help_string)
     gather = input("What would you like to do? ")
-    if gather.lower() == 'exit':
-        print("Exiting program.")
-        sys.exit()
-    if gather.lower() == "more":
-        webbrowser.open_new(site)
-        return
-    if gather.lower() == "return":
-        return
-    else:
-        print("not an option, oh well.")
-        return       
-
+    webbrowser.open_new(site) if gather.lower() == "more" else None
+    return
 
 if __name__ == "__main__":
-    main()
+    intro()
